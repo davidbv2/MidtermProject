b6456e41f9dbf45e83ea80ee49cf582ecac6bc73
David B Valentine II <davidbv2@gmail.com>
2017-08-02 19:49:45 -0400

    Added validation for pymt methods

--- src/MoneyHandler.java
+++ src/MoneyHandler.java
@@ -1,35 +1,40 @@
 public class MoneyHandler {
 
 
-        public static double cashTrans(double amountTendered, double grandTotal){
+        public static boolean cashTrans(double amountTendered, double grandTotal){
 
-            double change = amountTendered - grandTotal;
-
-
-            System.out.println("Your change is: " + change);
-            return  change;
+            if (amountTendered < grandTotal){
+                return false;
+            } else return true;
 
         }
-        public static void creditTrans(String ccNum, String expireDate, String cvvNum){
 
-            if (ccNum.length() != 16) {
-                System.out.println("Invalid card number, please reenter your card number");
-            }
-            if (expireDate.length() != 5){
-                System.out.println("Invalid expiration date, please reenter");
-            }
-            if (ccNum.length() != 3){
-                System.out.println("Invalid CVV, please reenter the number");
-            }
+        public static boolean creditTrans(String ccNum, String expireDate, String cvvNum) {
 
+            if ((ccNum.length() != 16) || (expireDate.length() != 5) || (cvvNum.length() != 3)) {
+                return false;
+            } else return true;
+        }
 
-            else System.out.println("Payment Accepted");
+        public static boolean checkTrans (String checkNum){
 
-
-
-
+            if ((checkNum.length() != 4)){
+                return false;
+            }else return true;
 
         }
+
+//
+//            if (ccNum.length() != 16) {
+//                System.out.println("Invalid card number, please reenter your card number");
+//            }
+//            if (expireDate.length() != 5){
+//                System.out.println("Invalid expiration date, please reenter");
+//            }
+//            if (ccNum.length() != 3){
+//                System.out.println("Invalid CVV, please reenter the number");
+//            }
+//
 
 
 
--- src/TruckApp.java
+++ src/TruckApp.java
@@ -1,3 +1,5 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,10 +15,12 @@ public class TruckApp {
         double grandSubTotal = 0.0;
         double SALESTAX = 1.06;
         String paymentMethod;
+        double amountTendered;
         String ccNum;
         String expireDate;
         String cvvNum;
-        int checkNum;
+        boolean notValid = false;
+        String checkNum;
         ArrayList<Food> userChoices = new ArrayList<Food>(); //use to store user choices
 
         Scanner scan = new Scanner(System.in);
@@ -51,12 +55,52 @@ public class TruckApp {
         System.out.println("What form of payment would you like to use: C=CASH, CC= CREDIT CARD, CHK= CHECK");
         paymentMethod = scan.nextLine();
 
-        if (paymentMethod.equalsIgnoreCase("C")){
-            System.out.println("How much cash are you tendering?: ");
-            double amountTendered = scan.nextDouble();
-            MoneyHandler.cashTrans(amountTendered,grandTotal);
-        } else if (paymentMethod.equalsIgnoreCase("CC")){
+        if (paymentMethod.equalsIgnoreCase("C")) {
+            do {
+                System.out.println("So.....how much money you got?: ");
+                amountTendered = scan.nextDouble();
+                if (amountTendered < grandTotal){
+                    double owed = grandTotal - amountTendered;
+                    System.out.println("Run me my money! You owe US! $" + owed);
+                }
+            } while (notValid == MoneyHandler.cashTrans(amountTendered, grandTotal));
 
+
+            if (MoneyHandler.cashTrans(amountTendered,grandTotal) == true){
+                double change = amountTendered - grandTotal;
+                System.out.println("I guess we'll give you back $ " + change + " in change.");
+            }
+
+        } else if (paymentMethod.equalsIgnoreCase("CC")) {
+
+            System.out.println("Helpful hint - if you do not enter the correct number of digits, you will be prompted to "
+                    + "re-enter all your payment information. Count with your fingers, if necessary.");
+            do {
+                System.out.print("Please enter your 16 digit credit card number: ");
+                ccNum = scan.nextLine();
+                System.out.print("Please enter your 4 digit expiration date (xx/xx): ");
+                expireDate = scan.nextLine();
+                System.out.print("Please enter your 3 digit CVV number: ");
+                cvvNum = scan.nextLine();
+
+            } while (notValid == MoneyHandler.creditTrans(ccNum, expireDate, cvvNum));
+
+            if (MoneyHandler.creditTrans(ccNum,expireDate,cvvNum) == true){
+                System.out.println("Your transaction was not declined!");
+            }
+        } else if (paymentMethod.equalsIgnoreCase("CHK")){
+
+            System.out.println("Helpful hint - if you do not enter a 4 digit check "
+                    + "number, you will be prompted to re-enter your check number. Count with your toes, if necessary.");
+
+            do {
+                System.out.print("Please enter your 4 digit check number: ");
+                checkNum = scan.nextLine();
+            } while (notValid == MoneyHandler.checkTrans(checkNum));
+
+            if (MoneyHandler.checkTrans(checkNum) == true){
+                System.out.println("Your check didn't bounce!");
+            }
         }
 
 
